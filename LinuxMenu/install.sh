0322185bcaa4b2c6ec4cef47dbeff9364e312d3a
Markus <contact@zgast.at>
2021-09-18 20:46:47 +0200

    un and install

---
+++ LinuxMenu/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo cp -r ./setting-scripts/ ~/.setting-scripts
+sudo chmod 777 -R ~/.setting-scripts/
+sudo cp -r ./LinuxMenu/ /opt/LinuxMenu
+sudo cp ./linux_menu.desktop /usr/share/applications/linux_menu.desktop
---
+++ LinuxMenu/uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo rm -rf ~/.setting-scripts
+sudo rm -rf /opt/LinuxMenu
+sudo rm /usr/share/applications/linux_menu.desktop
